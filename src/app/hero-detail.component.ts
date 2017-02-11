6bca53aa8066879e84f65b994bea827e9a17a945
Kalle Launiala <kalle.launiala@protonit.net>
2017-02-11 19:27:59 +0200

    Link to details routing

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -7,17 +7,8 @@ import "rxjs/add/operator/switchMap";
 
 @Component({
   selector: "my-hero-detail",
-  template: `
-<div *ngIf="hero">
-  <h2>{{hero.name}} details!</h2>
-  <div><label>id: </label>{{hero.id}}</div>
-  <div>
-    <label>name: </label>
-    <input [(ngModel)]="hero.name" placeholder="name">
-  </div>
-</div>
-<button (click)="goBack()">Back</button>
-`
+  templateUrl: "./hero-detail.component.html"
+
 })
 
 export class HeroDetailComponent implements OnInit {
