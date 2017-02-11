93080dac2b68699f5eb683899a92d91368bb0f4c
Kalle Launiala <kalle.launiala@protonit.net>
2017-02-11 17:21:02 +0200

    Another checkpoint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -79,7 +79,8 @@ export class AppComponent implements OnInit {
   }
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    //this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
   }
 
   title = 'Tour of Heroes';
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,7 +4,7 @@ import {HEROES} from "./mock-heroes";
 
 @Injectable()
 export class HeroService {
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Promise<Hero[]> {
+    return Promise.resolve(HEROES);
   }
 }
