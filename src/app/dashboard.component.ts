a20c75c4e9cd2b35272cc6f67b1c1a419703eda9
Kalle Launiala <kalle.launiala@protonit.net>
2017-02-11 19:40:22 +0200

    detail navigation

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -1,8 +1,8 @@
 
-
 import {Component, OnInit} from "@angular/core";
 import {Hero} from "./hero";
 import {HeroService} from "./hero.service";
+
 @Component({
   moduleId: module.id,
   selector: "my-dashboard",
