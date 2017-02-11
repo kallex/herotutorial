2d8a7fd0f1d2a8ae26bb079807eda541abc3c9d8
Kalle Launiala <kalle.launiala@protonit.net>
2017-02-11 19:48:51 +0200

    Component splitting

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -7,7 +7,8 @@ import {HeroService} from "./hero.service";
   moduleId: module.id,
   selector: "my-dashboard",
   //template: '<h3>My Dashboard</h3>'
-  templateUrl: "./dashboard.component.html"
+  templateUrl: "./dashboard.component.html",
+  styleUrls: [ "./dashboard.component.css"]
 })
 
 export class DashboardComponent implements OnInit {
--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -6,8 +6,10 @@ import {Location} from "@angular/common";
 import "rxjs/add/operator/switchMap";
 
 @Component({
+  moduleId: module.id,
   selector: "my-hero-detail",
-  templateUrl: "./hero-detail.component.html"
+  templateUrl: "./hero-detail.component.html",
+  styleUrls: [ "./hero-detail.component.css"]
 
 })
 
