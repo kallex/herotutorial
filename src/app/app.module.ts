f35a192c0047901a1688a8b97718efb22e1d7ae7
Kalle Launiala <kalle.launiala@protonit.net>
2017-02-11 22:41:23 +0200

    http with inmemorydb implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,30 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { InMemoryWebApiModule } from "angular-in-memory-web-api";
+
 import {HeroesComponent} from './heroes.component';
 import {HeroDetailComponent} from "./hero-detail.component";
 import {AppComponent} from "./app.component";
 import {HeroService} from "./hero.service";
 import {DashboardComponent} from "./dashboard.component";
 import {AppRoutingModule} from "./app-routing.module";
+import {InMemoryDataService} from "./in-memory-data.service";
 
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+  ],
   declarations: [
     AppComponent,
     DashboardComponent,
     HeroesComponent,
     HeroDetailComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    AppRoutingModule
   ],
   providers: [ HeroService],
   bootstrap: [AppComponent]
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,12 +1,28 @@
 import {Injectable} from "@angular/core";
 import {Hero} from "./hero";
-import {HEROES} from "./mock-heroes";
+import {Http} from "@angular/http";
+import "rxjs/add/operator/toPromise"
 
 @Injectable()
 export class HeroService {
-  getHeroes(): Promise<Hero[]> {
-    return Promise.resolve(HEROES);
+  private heroesurl = "api/heroes";
+
+  constructor(private http: Http) {
+
   }
+
+  getHeroes(): Promise<Hero[]> {
+    return this.http.get(this.heroesurl)
+      .toPromise()
+      .then(response => response.json().data as Hero[])
+      .catch(this.handleError);
+  }
+
+  private handleError(error:any) : Promise<any> {
+    console.error("An error occurred", error);
+    return Promise.reject(error.message || error);
+  }
+
   getHeroesSlowly(): Promise<Hero[]> {
     return new Promise(resolve => {
       setTimeout(() => resolve(this.getHeroes()), 2000);
