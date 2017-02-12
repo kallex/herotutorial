bebb044cf5f5532d73f64ae244e7d8e78fe84b85
Kalle Launiala <kalle.launiala@protonit.net>
2017-02-12 07:52:42 +0200

    Delete hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -55,4 +55,12 @@ export class HeroService {
       .then(res => res.json().data)
       .catch(this.handleError);
   }
+
+  delete(id: number) : Promise<void> {
+    const url = `${this.heroesurl}/${id}`;
+    return this.http.delete(url, {headers: this.headers})
+      .toPromise()
+      .then(() => null)
+      .catch(this.handleError);
+  }
 }
