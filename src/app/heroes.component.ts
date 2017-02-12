e1e6caa26867f6c55b59414fd7692f14f629ce52
Kalle Launiala <kalle.launiala@protonit.net>
2017-02-12 11:34:53 +0200

    Rewired animations, imported lodash

--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -1,14 +1,30 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, Input, trigger, state, style, transition, animate} from '@angular/core';
 import {Hero} from "./hero";
 import {HeroService} from "./hero.service";
 import {Router} from "@angular/router";
 
+import * as _ from "lodash";
 
 @Component({
   selector: 'my-heroes',
   //templateUrl: './app.component.html',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
+  animations: [
+    trigger("heroState", [
+      state("inactive", style([{
+        backgroundColor: "#eee",
+        transform: "scale(1)"
+      }
+      ])),
+      state("active", style([{
+        backgroundColor: "#cfd8dc",
+        transform: "scale(1.1)"
+      }])),
+      transition("inactive => active", animate("100ms ease-in")),
+      transition("active => inactive", animate("100ms ease-out"))
+    ])
+  ]
 })
 export class HeroesComponent implements OnInit {
   ngOnInit(): void {
@@ -52,9 +68,23 @@ export class HeroesComponent implements OnInit {
       });
   }
 
+  toggleState(hero: Hero) : void  {
+    if(hero.state === "active")
+      hero.state = "inactive";
+    else
+      hero.state = "active";
+  }
+
+  setState(hero: Hero, state: string) : void {
+    hero.state = state;
+  }
+
+
   selectedHero: Hero;
   heroes : Hero[];
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    _.forEach(this.heroes, item => this.setState(item, "inactive"));
+    this.setState(hero, "active");
   }
 }
